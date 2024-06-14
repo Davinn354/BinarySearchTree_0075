9abfd53f0e2f66bac3f332fb842c7e0df377d2f5
Davinn354 <daviipad53@gmail.com>
2024-06-14 13:58:01 +0700

    Penambahan node, search untuk mengalokasinya, serta penambahan if dan else if ( jika if Nol pohon kosong)

--- BinarySearchTree_0075/BinarySearchTree_0075.cpp
+++ BinarySearchTree_0075/BinarySearchTree_0075.cpp
@@ -17,7 +17,6 @@ public:
 
     }
 };
-
 class BinaryTree
 {
 public:
@@ -25,7 +24,34 @@ public:
 
     BinaryTree()
     {
-        ROOT = NULL; // Initializing root to null
+        ROOT = nullptr; // initializing ROOT to null
     }
-};
 
+    void insert(string element) // inseret a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); //  allocate memory for the new node
+        newNode->info = element; // assighn value to the data field of the new node
+        newNode->leftchild = nullptr; // make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // make the right child of the new node point to NULL
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // locate the node which will bw the parent of the node to ins
+
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as ROOT
+            return; // exit
+        }
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that
+        {
+            parent->rightchild = newNode; // make the left child of the parent point to the new node
+        }
+    }
+
+    
+};
